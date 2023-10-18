c4e4782c3d63ec9986961f5b3ac82eee6e37e80b
komori3 <39507080+komori3@users.noreply.github.com>
2023-10-18 16:00:55 +0900

    show bests and uniques

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -9,44 +9,74 @@ SUBMISSIONS_DIR = os.path.join(ROOT_DIR, 'submissions')
 
 
 
-def show_standings(dict_submission_to_total_score):
-    max_length = len('submission')
-    list_total_score_to_submission = []
-    for submission, total_score in dict_submission_to_total_score.items():
-        max_length = max(max_length, len(submission))
-        list_total_score_to_submission.append((total_score, submission))
+def show_standings(tag_to_total_score, tag_to_bests, tag_to_uniques):
 
-    list_total_score_to_submission.sort()
+    max_tag_length = len('submission')
+    max_score_length = len('score')
+    max_bests_length = len('bests')
 
-    space = max_length - len('submission') + 4
-    print('submission' + (' ' * space) + 'score')
-    print('-' * 50)
-    for total_score, submission in list_total_score_to_submission:
-        space = max_length - len(submission) + 4
-        print(submission + (' ' * space) + str(total_score))
+    list_total_score_to_tag = []
+    for tag, total_score in tag_to_total_score.items():
+        list_total_score_to_tag.append((total_score, tag))
+        max_tag_length = max(max_tag_length, len(tag))
+        max_score_length = max(max_score_length, len(str(total_score)))
+        max_bests_length = max(max_bests_length, len(str(tag_to_bests[tag])))
+
+    list_total_score_to_tag.sort()
+
+    space_tag = max_tag_length - len('submission') + 4
+    space_score = max_score_length - len('score') + 4
+    space_bests = max_bests_length - len('bests') + 4
+
+    print('submission' + (' ' * space_tag) + 'score' + (' ' * space_score) + 'bests' + (' ' * space_bests) + 'uniques')
+    print('-' * 100)
+    for total_score, tag in list_total_score_to_tag:
+        bests = tag_to_bests[tag]
+        uniques = tag_to_uniques[tag]
+        space_tag = max_tag_length - len(tag) + 4
+        space_score = max_score_length - len(str(total_score)) + 4
+        space_bests = max_bests_length - len(str(bests)) + 4
+        print(tag + (' ' * space_tag) + str(total_score) + (' ' * space_score) + str(bests) + (' ' * space_bests) + str(uniques))
 
 if __name__ == "__main__":
 
-    submission_to_results = {}
+    tag_to_results = {}
     for tag in os.listdir(SUBMISSIONS_DIR):
         results_file = os.path.join(SUBMISSIONS_DIR, tag, 'results.yaml')
         with open(results_file) as f:
-            submission_to_results[tag] = yaml.safe_load(f)
+            tag_to_results[tag] = yaml.safe_load(f)
 
-    seed_best = defaultdict(lambda: 1e20)
-    for tag, results in submission_to_results.items():
+    seed_to_best_score = defaultdict(lambda: 1e20)
+    seed_to_unique_tag = defaultdict(lambda: '')
+    for tag, results in tag_to_results.items():
         for result in results:
-            seed_best[result['Seed']] = min(seed_best[result['Seed']], result['Score'])
+            seed, score = result['Seed'], result['Score']
+            if score == 0: continue
+            if score == seed_to_best_score[seed]:
+                seed_to_unique_tag[seed] = ''
+            if score < seed_to_best_score[seed]:
+                seed_to_best_score[seed] = score
+                seed_to_unique_tag[seed] = tag
 
-    dict_submission_to_total_score = defaultdict(lambda: 0.0)
-    for tag, results in submission_to_results.items():
+    tag_to_total_score = defaultdict(lambda: 0.0)
+    tag_to_bests = defaultdict(lambda: 0)
+    tag_to_uniques = defaultdict(lambda: 0)
+
+    for tag, results in tag_to_results.items():
         ctr = 0
         for result in results:
             ctr += 1
-            if result['Score'] == -1: continue
+            seed, score = result['Seed'], result['Score']
+            if score == 0: continue
             # dict_submission_to_total_score[tag] += seed_best[result['Seed']] / result['Score']
             # dict_submission_to_total_score[tag] += math.log(result['Score'])
-            dict_submission_to_total_score[tag] += result['Score']
-        dict_submission_to_total_score[tag] /= ctr
+            tag_to_total_score[tag] += score
+            if score == seed_to_best_score[seed]:
+                tag_to_bests[tag] += 1
+        tag_to_total_score[tag] /= ctr
+    
+    for seed, tag in seed_to_unique_tag.items():
+        if tag == '': continue
+        tag_to_uniques[tag] += 1
 
-    show_standings(dict_submission_to_total_score)
+    show_standings(tag_to_total_score, tag_to_bests, tag_to_uniques)
